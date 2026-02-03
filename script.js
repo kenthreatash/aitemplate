2e815f05c58b879bf335775bbaa1f7d6e42ae89b
kenthreatash <threatash@gmail.com>
2026-02-03 05:51:38 +0100

    Refactor event listeners for attendance and diet

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ const noSection = document.getElementById("noSection");
 const diet = document.getElementById("diet");
 const dietDetails = document.getElementById("dietDetails");
 
+const children = document.getElementById("children");
+const childrenDetails = document.getElementById("childrenDetails");
+
 attendance.addEventListener("change", () => {
   yesSection.classList.add("hidden");
   noSection.classList.add("hidden");
 
-  if (attendance.value === "Igen") {
-    yesSection.classList.remove("hidden");
-  }
-
-  if (attendance.value === "Nem") {
-    noSection.classList.remove("hidden");
-  }
+  if (attendance.value === "Igen") yesSection.classList.remove("hidden");
+  if (attendance.value === "Nem") noSection.classList.remove("hidden");
 });
 
 diet.addEventListener("change", () => {
-  dietDetails.classList.toggle(
-    "hidden",
-    diet.value !== "Van"
-  );
+  dietDetails.classList.toggle(diet.value !== "Van");
+});
+
+children.addEventListener("change", () => {
+  childrenDetails.classList.toggle(children.value !== "Van");
 });
